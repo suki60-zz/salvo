6637622455d5e5e11d6d7767a19693764544d68d
Francesc Altés <suki60@Francescs-MacBook.local>
2018-01-15 11:27:34 +0100

    front-end of ships implement + game player info (TASK 3 DONE)

--- src/main/resources/static/web/js/game.js
+++ src/main/resources/static/web/js/game.js
@@ -1,6 +1,25 @@
-var game_player = paramObj();
+var query_obj = getQueryObj();
 
-function paramObj() {
+console.log(query_obj.gp);
+
+$.getJSON("/api/game_view/" + query_obj.gp)
+  .done(function(game) {
+
+    console.log(game);
+
+    $("#player-info-title").text(createPlayerInfo(game));
+
+    createBoard();
+
+    createShips(game);
+  })
+  .fail(function(jqXHR, textStatus, errorThrown) {
+    console.log("fail");
+  })
+
+// functions
+
+function getQueryObj() {
   var search = location.search;
   var obj = {};
   var reg = /(?:[?&]([^?&#=]+)(?:=([^&#]*))?)(?:#.*)?/g;
@@ -11,39 +30,64 @@ function paramObj() {
 
   return obj;
 }
-console.log(game_player.gp);
 
-$.getJSON("/api/game_view/" + game_player.gp, function(data) {})
-  .done(function(data) {
+function createPlayerInfo(game) {
 
-    console.log("Ok");
-    console.log(data);
-  })
-  .fail(function(jqXHR, textStatus, errorThrown) {
-    console.log("fail");
-  })
+  var viewer = "";
+  var opponent = "";
+  var viewer_id = getQueryObj().gp;
 
-// create cells for table
-var letter = ["Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+  $(game.gamePlayers).each(function(i, gamePlayer) {
 
-for (var i = 0; i < letter.length; i++) {
+    if(gamePlayer.id == viewer_id) {
 
-  $('<tr/>', {
-    id: letter[i],
-  }).appendTo("#board-table");
+      viewer = gamePlayer.players.email + "(you)";
+    } else {
 
-  for (var j = 0; j < letter.length; j++) {
-
-    $('<td/>', {
-      id: letter[i] + j,
-      class: "cell",
-    }).appendTo("#" + letter[i]);
-
-    if ((j == 0) && (i != 0)) {
-      $("#" + letter[i] + j).text(i);
+      opponent = gamePlayer.players.email;
     }
-    if((i == 0) && (j != 0)) {
-      $("#" + letter[i] + j).text(letter[j]);
+  })
+
+  return viewer + " vs " + opponent;
+}
+
+function createBoard() {
+
+  var letter = ["Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+
+  for (var i = 0; i < letter.length; i++) {
+
+    $('<tr/>', {
+      id: letter[i],
+    }).appendTo("#board-table");
+
+    for (var j = 0; j < letter.length; j++) {
+
+      $('<td/>', {
+        id: letter[i] + j,
+        class: "cell",
+      }).appendTo("#" + letter[i]);
+
+      if ((j == 0) && (i != 0)) {
+        $("#" + letter[i] + j).text(i);
+      }
+      if ((i == 0) && (j != 0)) {
+        $("#" + letter[i] + j).text(letter[j]);
+      }
+      if (!((i == 0) || (j == 0))) {
+        $("#" + letter[i] + j).attr("bgcolor", "#3498DB");
+      }
     }
   }
+}
+
+function createShips(game) {
+
+  $(game.ships).each(function(i, ship) {
+
+    $(ship.location).each(function(i, cell) {
+
+      $("#" + cell).attr("bgcolor", "#707B7C");
+    })
+  })
 }
